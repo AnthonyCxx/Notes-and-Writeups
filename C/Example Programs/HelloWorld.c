e2e77786b371370fc89327acab54d4768309db49
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-06-30 17:23:37 -0400

    Update HelloWorld.c

--- C/Example Programs/HelloWorld.c
+++ C/Example Programs/HelloWorld.c
@@ -1,11 +1,10 @@
-```C
-#include <stdio.h>        // C version of <iostream>
+// C version of <iostream>
+#include <stdio.h>       
 
-int main(void)           //'void' is not necessary, but good practice in C
+int main(void)        //'void' is not necessary, but good practice in C
 {
   // %s is the string format conversion specifier, replaced by 'Hello, World!\n'
   printf("%s", "Hello, world!\n");           
 
   return 0;
 }
-```
