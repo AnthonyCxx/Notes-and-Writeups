23c9e6d068dee4dbc2a7106a1c1d4f3c21607f03
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-07-18 19:45:04 -0400

    Create Char : Get Single Character.cpp

---
+++ C++/Code Snippets/Char : Get Single Character.cpp
@@ -0,0 +1,22 @@
+/*
+Generally, you can use 'cin.get()', but even that leaves a newline ('\n') character in the stream, which can affect your program.
+This gave me a problem when I was making my Hangman game -- I needed raw input, without the newline.
+*/
+
+#include <limits>        //Contains numeric_limits::max()
+
+// Get a single character, disposing of the input from the 'enter' key
+char getChar()
+{
+    char character;
+
+    //Get character from the user
+    cout << "Enter a character: ";
+    cin.get(character);
+
+    //Prevent registering hitting 'enter' to submit guess as another guess
+    cin.clear();                                                 //Always call 'cin.clear()' before 'cin.ignore()'
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');        // Ignore everything up to the user pressing 'enter'
+
+    return character;
+}
