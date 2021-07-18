96c59b6ae049a147a907d78427b2a522132148d4
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-07-18 19:08:05 -0400

    Update Generic : Reverse Iterable Object.cpp

--- C++/Code Snippets/Generic : Reverse Iterable Object.cpp
+++ C++/Code Snippets/Generic : Reverse Iterable Object.cpp
@@ -4,11 +4,12 @@ Type reverse(const Type& iterObj)  //Any iterable rvalue
     Type reversed = iterObj;         //A copy of the item
     size_t size = iterObj.size();   //Reduces code clutter
 
+    //Swap each item
     for(int i=0; i < (size / 2); i++)
     {
         swap(reversed[i], reversed[size-1-i]);
     }
 
-    //Return 
+    //Return reversed object
     return reversed;
 }
