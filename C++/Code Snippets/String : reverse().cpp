2b3dcb3a84b7edf61226fc27525cdc0e50002a58
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-07-18 18:52:30 -0400

    Update String : reverse().cpp

--- C++/Code Snippets/String : reverse().cpp
+++ C++/Code Snippets/String : reverse().cpp
@@ -1,14 +1,3 @@
-// Reverses an existing string
-void reverse(string& str)
-{
-    size_t size = str.length();
-
-    for (int i=0; i < size / 2; i++)
-    {
-        swap(str[i], str[size - i - 1]);
-    }
-}
-
 // Returns a new string (can be used in expressions like 'str == reverse(str)')
 string reverse(const string& source)
 {
