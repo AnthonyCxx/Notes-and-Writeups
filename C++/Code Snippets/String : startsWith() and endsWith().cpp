664c5b06c9308af4fc14fdc5fdc53fda4188cb22
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-07-18 17:21:27 -0400

    Update String : startsWith() and endsWith().cpp

--- C++/Code Snippets/String : startsWith() and endsWith().cpp
+++ C++/Code Snippets/String : startsWith() and endsWith().cpp
@@ -1,3 +1,6 @@
+// Made in C++20, compile with 'g++ -std=c++2a'
+#include <string_view>
+
 bool startsWith(const string_view text, const string_view searchStr)
 {
     return text.find(searchStr) == 0;
