06f5f9ddaf7bff43fefff50b713e5700cb005643
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-07-18 20:11:46 -0400

    Update String : startsWith() and endsWith().cpp

--- C++/Code Snippets/String : startsWith() and endsWith().cpp
+++ C++/Code Snippets/String : startsWith() and endsWith().cpp
@@ -1,5 +1,9 @@
- // Made in C++20, compile with 'g++ -std=c++2a'
-// '<string_view>' is a C++20 header file that increases efficiency
+/*
+'<string_view>' is a C++17 header file that increases efficiency
+
+If you don't want to use C++17, replace 'string_view' with 'string&'
+*/
+
 #include <string_view>
 
 bool startsWith(const string_view text, const string_view searchStr)
