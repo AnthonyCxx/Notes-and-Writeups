ce08b03ebc3fc4bdc5a78609a9f59a029b3efd26
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-07-19 23:58:46 -0400

    Create String: wordCount().cpp

---
+++ C++/Code Snippets/String: wordCount().cpp
@@ -0,0 +1,17 @@
+// Inspired by this GeeksforGeeks article: https://www.geeksforgeeks.org/stringstream-c-applications/
+
+#include <string>
+#include <sstream>
+
+// Count the amount of words in a string
+int wordCount(const string& str)
+{
+    stringstream text(str);
+    string word;
+    int count = 0;
+
+    // For every word in the string stream, increment the count
+    while (text >> word) count++;
+
+    return count;
+}
