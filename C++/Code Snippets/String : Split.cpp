edc860552e413db45c03e3be8ad2f676229f9d00
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-07-19 23:13:19 -0400

    Create String : Split.cpp

---
+++ C++/Code Snippets/String : Split.cpp
@@ -0,0 +1,26 @@
+// Split a string with a delimiter
+#include <string>
+#include <vector>
+
+vector<string> split(const string& text, const char delimiter)
+{
+    vector<string> split_text;
+    string word;
+
+    // For every character in the text
+    for(int i=0; i < text.length(); i++)
+    {
+        if (text[i] == delimiter)
+        {
+            split_text.push_back(word);
+            word.clear();
+        }
+        else
+            word += text[i];   // C++ strings are mutable, so this isn't a problem
+    }
+
+    // Append the last word
+    split_text.push_back(word);
+
+    return split_text;
+}
