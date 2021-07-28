17eb91e9ffd68326dfea12427b95045baaf6e9bc
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-07-28 15:15:14 -0400

    Update String : isPalindrome().cpp

--- C++/Code Snippets/String : isPalindrome().cpp
+++ C++/Code Snippets/String : isPalindrome().cpp
@@ -1,13 +1,7 @@
-/*
-'<string_view>' is a C++17 header file that increases efficiency
-
-If you don't want to use C++17, replace 'string_view' with 'string&'
-*/
-
-#include <string_view>
+#include <string>
 
 // Reverses a string, returning a new string
-string reverse(const string_view source)
+string reverse(const string& source)
 {
     string reversed;
 
@@ -20,7 +14,7 @@ string reverse(const string_view source)
 }
 
 // A palindrome is a word that is spelled the same backwards and forwards
-bool isPalindrome(const string_view str)
+bool isPalindrome(const string& str)
 {
     return str == reverse(str);
 }
