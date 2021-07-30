9a6242dc496fc3dd04f0fcbfcc1733a8fc00bd7e
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-07-30 19:09:18 -0400

    Create General : Time Duration of Work.cpp

---
+++ C++/Code Snippets/General : Time Duration of Work.cpp
@@ -0,0 +1,16 @@
+//How to time to stuff like functions in C++, taken from Howard Hinnant's 'A ＜chrono> Tutorial'
+#include <chrono>
+using namespace std::chrono;
+
+//Get start time
+auto start = steady_clock::now();
+
+/*
+  WORK GOES HERE
+*/
+
+//Get end time
+auto end = steady_clock::now();
+
+//Output total time between time_point objects
+cout << seconds{end - start}.count() << "seconds\n";
