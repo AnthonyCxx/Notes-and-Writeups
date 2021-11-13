f2d99d3f28b8023d9753b8bbac67a4f1bd70af9f
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-11-13 18:00:58 -0500

    Update Fisher and Yates Randomizer.cpp

--- C++/Data Structures and Algorithms/Randomization/Fisher and Yates Randomizer.cpp
+++ C++/Data Structures and Algorithms/Randomization/Fisher and Yates Randomizer.cpp
@@ -1,3 +1,5 @@
+//O(n) runtime
+
 #include <iostream>
 
 //C libraries (for creating + seeding the RNG)
@@ -20,10 +22,11 @@ void randomize(T arr[], std::size_t n)  //Array + its size
     //A random index to swap 'i' with
     std::size_t randomIndex;
 
+     //For every item in the array (n)
     for(std::size_t i=0; i < n; ++i)
     {
-        randomIndex = rand() % n;
-        swap<T>(arr[i], arr[randomIndex]);
+        randomIndex = rand() % n;               //Generate a random index
+        swap<T>(arr[i], arr[randomIndex]);     //Swap the the two indices
     }
 }
 
