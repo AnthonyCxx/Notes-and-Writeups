5f6c7d88953f2de1a11d7fcf589479fad4c7b5ff
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-11-13 18:21:33 -0500

    Update Random Selection.cpp

--- C++/Data Structures and Algorithms/Randomization/Random Selection.cpp
+++ C++/Data Structures and Algorithms/Randomization/Random Selection.cpp
@@ -1,10 +1,12 @@
-//A simple random selection algorithm
+//A simple random selection algorithm using MT19937 (C++'s implementation of the Mersenne Twister algorithm)
 
 T randomElement(T arr[], std::size_t n)
 {
     //Create and seed a random number generator (Mersenne Twister)
     static std::random_device rd;
-    static std::mt19937 RNG(rd());                                                                                                                                                                                                                                                                                          //Apply distribution (0 - n-1), inclusive
+    static std::mt19937 RNG(rd());            
+    
+    //Apply distribution (0 - n-1), inclusive
     std::uniform_int_distribution<int> dist(0, n-1);
 
     //Return a random element using the applied distribution
