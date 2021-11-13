72c92207dbbfeffbca23bf96ee2d9634f708654f
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-11-13 17:56:44 -0500

    Update Fisher and Yates Randomizer.cpp

--- C++/Data Structures and Algorithms/Randomization/Fisher and Yates Randomizer.cpp
+++ C++/Data Structures and Algorithms/Randomization/Fisher and Yates Randomizer.cpp
@@ -29,36 +29,11 @@ void randomize(T arr[], std::size_t n)  //Array + its size
 
 
 
-
-
-//Unncessary function
-template <typename T>
-void printArray(T arr[], std::size_t n)
-{
-    for(std::size_t i=0; i < n; ++i)
-    {
-        std::cout << arr[i] << ' ';
-    }
-    std::cout << '\n';
-}
-
-
-
-
-
-
 // DRIVER CODE //
 int main()
 {
     srand(time(NULL));
-
-    int data[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
-    printArray(data, 10);
     randomize(data, 10);
-
-    std::cout << '\n';
-    printArray(data, 10);
 
     return 0;
 }
