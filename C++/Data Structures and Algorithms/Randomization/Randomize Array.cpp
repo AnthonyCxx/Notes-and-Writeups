ae25d38a31218f019bf0434ec39a844711c9aad4
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-11-13 17:26:55 -0500

    Create Randomize Array.cpp

---
+++ C++/Data Structures and Algorithms/Randomization/Randomize Array.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+//C libraries (for creating + seeding the RNG)
+#include <cstdlib>
+#include <time.h>
+
+template <typename T>
+void swap(T& a, T& b)
+{
+    T temp(a);
+    a = b;
+    b = temp;
+}
+
+//Randomizes an array
+template <typename T>
+void randomize(T arr[], std::size_t n)  //Array + its size
+{
+    //A random index to swap 'i' with
+    std::size_t randomIndex;
+
+    for(std::size_t i=0; i < n; ++i)
+    {
+        randomIndex = rand() % n;
+        swap<T>(arr[i], arr[randomIndex]);
+    }
+}
+
+
+template <typename T>
+void printArray(T arr[], std::size_t n)
+{
+    for(std::size_t i=0; i < n; ++i)
+    {
+        std::cout << arr[i] << ' ';
+    }
+    std::cout << '\n';
+}
+
+// DRIVER CODE //
+int main()
+{
+    srand(time(NULL));
+
+    int data[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    printArray(data, 10);
+    randomize(data, 10);
+
+    std::cout << '\n';
+    printArray(data, 10);
+
+    return 0;
+}
