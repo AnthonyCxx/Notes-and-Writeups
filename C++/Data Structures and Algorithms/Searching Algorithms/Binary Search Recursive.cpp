97e28b0a6683d14de4bc456f912c3a9cda38568a
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-11-14 00:57:17 -0500

    Update Binary Search Recursive.cpp

--- C++/Data Structures and Algorithms/Searching Algorithms/Binary Search Recursive.cpp
+++ C++/Data Structures and Algorithms/Searching Algorithms/Binary Search Recursive.cpp
@@ -7,3 +7,27 @@
   Time Complexity: O(log n)
   Space Complexity: O(log n)
 */
+
+template <typename T>    //Array,        left side,          right side,          search value
+std::size_t binarySearch(const T arr[], const std::size_t l, const std::size_t r, const T key)
+{
+    if (l <= r)
+    {
+        //Calculate the midpoint
+        std::size_t mid = (l + r) / 2;
+
+        //Search value found?
+        if (key == arr[mid])
+            return mid;    //Return the index of the key
+
+        //Otherwise cut in half and start over
+        else if (key < arr[mid])    //Discards the greater half
+            return binarySearch<T>(arr, l, mid-1, key);
+
+        else if (key > arr[mid])    //Discards the smaller half
+            return binarySearch<T>(arr, mid+1, r, key);
+    }
+
+    //If list is exhausted without finding the value, then return -1 (sentinel value)
+    return -1;
+}
