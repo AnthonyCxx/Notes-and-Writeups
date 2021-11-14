3d05e35895dbba94b09fa378a4ef2462bd346461
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-11-14 00:38:50 -0500

    Update Sequential Search.cpp

--- C++/Data Structures and Algorithms/Searching Algorithms/Sequential Search.cpp
+++ C++/Data Structures and Algorithms/Searching Algorithms/Sequential Search.cpp
@@ -1,1 +1,13 @@
+template <typename T>
+std::size_t sequentialSearch(T arr[], std::size_t n, T key)
+{
+    //Interate over every element in the array
+    for(std::size_t i=0; i < n; ++i)
+    {
+        if (arr[i] == key)  //If you find the item, return it
+            return i;
+    }
 
+    //If list is exhausted, return -1 (sentinel value)
+    return -1;
+}
