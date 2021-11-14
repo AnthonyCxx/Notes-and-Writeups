ea7831c77106828a605747ae92eb0a119f655a79
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-11-14 00:48:37 -0500

    Update Binary Search Iterative.cpp

--- C++/Data Structures and Algorithms/Searching Algorithms/Binary Search Iterative.cpp
+++ C++/Data Structures and Algorithms/Searching Algorithms/Binary Search Iterative.cpp
@@ -1,4 +1,35 @@
 /*
+  Templated, Iterative Binary Search
+  ==================================
+
   Time Complexity: O(1)
   Space Complexity: O(log n)
 */
+
+template <typename T>
+std::size_t binarySearch(const T arr[], const std::size_t n, const T key)
+{
+    //Initial points for left and right (and declaration of midpoint)
+    std::size_t left = 0, mid, right = n-1;
+
+    //Search loop (continues until list is exhausted)
+    while (left <= right)   //<== Must be '<=' otherwise it will end before final step
+    {
+        //Calculate the midpoint of the array
+        mid = (left + right) / 2;
+
+        //Search value found?
+        if (arr[mid] == key)
+            return mid;       //Return the index of the key
+
+        //Otherwise, cut in half and start over
+        else if (key < arr[mid])   //Discards the greater half
+            right = mid-1;
+
+        else if (key > arr[mid])  //Discards the smaller half
+            left = mid+1;
+    }
+
+    //If the list is exhausted without finding the value, then return -1 (sentinel value)
+    return -1;
+}
