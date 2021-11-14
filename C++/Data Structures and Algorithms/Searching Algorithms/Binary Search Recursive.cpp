2a4187b2389aeb84b28b3c7554cfc7365c78fb5a
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-11-14 00:51:50 -0500

    Update Binary Search Recursive.cpp

--- C++/Data Structures and Algorithms/Searching Algorithms/Binary Search Recursive.cpp
+++ C++/Data Structures and Algorithms/Searching Algorithms/Binary Search Recursive.cpp
@@ -2,7 +2,7 @@
   Templated, Recursive Binary Search
   =================================
   
-  Requirements: data is sorted
+  RREQUIREMENTS: data is sorted
 
   Time Complexity: O(log n)
   Space Complexity: O(log n)
