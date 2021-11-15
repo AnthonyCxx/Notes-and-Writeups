cfba13cbe1fa2cba27307b7792ea1f5d3086c192
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-11-14 21:56:31 -0500

    Update Selection Sort.cpp

--- C++/Data Structures and Algorithms/Sorting Algorithms/Selection Sort.cpp
+++ C++/Data Structures and Algorithms/Sorting Algorithms/Selection Sort.cpp
@@ -11,19 +11,16 @@ void swap(T& a, T& b)
 template <typename T>
 void selectionSort(T array[], const std::size_t n)
 {
-    //To keep track of the smallest number in the array
     std::size_t min;
 
     //For every element in the array
-    for(size_t i=0; i < n-1; ++i)
+    for (std::size_t i = 0; i < n-1; ++i)
     {
-        //Set the 'smallest' number to the current index
         min = i;
 
         //Starting at the current index, iterate over the rest of the indices until the array is exhausted
-        for(size_t j=i+1; j < n; ++j)
+        for (std::size_t j=i+1; j < n; ++j)
         {
-            //If you come across a smaller number while iterating, set it to the smallest number
             if (array[j] < array[min])
                 min = j;
         }
