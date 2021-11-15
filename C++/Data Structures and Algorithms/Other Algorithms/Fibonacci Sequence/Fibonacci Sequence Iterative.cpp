af4e5abab87e61f33480c30ac720efe6edf8f658
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-11-14 22:22:05 -0500

    Create Fibonacci Sequence Iterative.cpp

---
+++ C++/Data Structures and Algorithms/Other Algorithms/Fibonacci Sequence/Fibonacci Sequence Iterative.cpp
@@ -0,0 +1,19 @@
+//Iterative fibonacci sequence
+unsigned long long fib(unsigned long long n)
+{
+    unsigned long long previous = 1, current = 1, next;
+
+    //If 'n' is 0 or 1, return the appropriate number
+    if (n <= 1)
+        return n;
+
+    //Calculate the Fibonacci number
+    for(int i = 3; i <= n; i++)  //Starting at the third iteration (skips the first two)
+    {
+        next = previous + current;
+        previous = current;
+        current = next;
+    }
+
+    return next;
+}
