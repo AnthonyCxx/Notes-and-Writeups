e9b5608e69e195c5d2197510a239dec82748ac2b
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-11-14 21:53:47 -0500

    Update Exchange Sort.cpp

--- C++/Data Structures and Algorithms/Sorting Algorithms/Exchange Sort.cpp
+++ C++/Data Structures and Algorithms/Sorting Algorithms/Exchange Sort.cpp
@@ -9,16 +9,23 @@ void swap(T& a, T& b)
 
 //Exchange sort
 template <typename T>
-void exchangeSort(T array[], const size_t size)
+void exchangeSort(T array[], const std::size_t n)
 {
+    bool notSorted = true;
+
     //For every element in the array
-    for(size_t i=0; i < size-1; i++)
+    for(std::size_t i=0; i < n-1 && notSorted; ++i)
     {
+        notSorted = false;
+
         //Compare every element after it to the current element (i), swapping if needed
-        for(size_t j=i+1; j < size; j++)
+        for(std::size_t j=i+1; j < n; ++j)
         {
             if (array[i] > array[j])
+            {
                 swap<T>(array[i], array[j]);
+                notSorted = true;
+            }
         }
     }
 }
