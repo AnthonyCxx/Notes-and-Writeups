2956e50d4c5e1f93a2e42a9d9f265a8b9365cdb8
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-11-15 15:04:57 -0500

    Update Custom atoi() Function.cpp

--- C++/Data Structures and Algorithms/Miscellaneous/Custom atoi() Function.cpp
+++ C++/Data Structures and Algorithms/Miscellaneous/Custom atoi() Function.cpp
@@ -13,7 +13,7 @@ template <typename returnType = int>
     //Custom implementation of C's 'isdigit()' from the <cctype> library, but using constexpr and noexcept for better performance
     auto isNumericChar = [](const char c) constexpr noexcept -> bool { return c >= 48 and c <= 57; };
 
-    //Return nullopt if not all the characters are 0-9 (with the exception of the first character, which may be a positive/negative sign or a number)
+    //Return nullopt if not all the characters are 0-9 (with the exception of the first character, which may also be a positive/negative sign or a number)
     if ( (str[0] == '-' or str[0] == '+' or isNumericChar(str[0]))   and   !std::all_of(str.cbegin()+1, str.cend(), isNumericChar) )
         return std::nullopt;
 
