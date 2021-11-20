3a2bece328ea35046a7bf2b09dcc84d0c1c67031
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-11-20 11:33:08 -0500

    Create 7. Reverse Integer.cpp

---
+++ C++/Data Structures and Algorithms/Leetcode/7. Reverse Integer.cpp
@@ -0,0 +1,48 @@
+/*
+  Solution explanation (solution 1): https://stackoverflow.com/questions/15355080/the-most-efficient-way-to-reverse-a-number
+  Solution explanation (solution 2): https://www.programiz.com/cpp-programming/examples/reverse-number
+*/
+
+
+/*
+  SOLUTION 1: Casting to std::string
+  ==================================
+  
+  This is a fairly straight-forward solution. Cast the integer
+  to a string and then reverse it character by character. If you 
+  get an error when casting it back to an integer, then you know
+  that the number is not a valid int.
+*/
+int reverse(int x) 
+{
+    //String representation of the integer
+    std::string as_str = to_string(x);
+
+    //Iterate over the string, swapping every step. Skip first index if negative.
+    for(int i=(x < 0 ? 1 : 0), j=as_str.size()-1 ; i < j; ++i, --j)
+    {
+        //Swap the current index with its mirror
+        std::swap(as_str[i], as_str[j]);
+    }
+
+    //Get the result as an integer
+    try
+    {
+          //The stoi() function ONLY returns integers and throws an error if it's out of range. Use std::stol and std::stoll for longs/long longs
+          return stoi(as_str);  //breaks here if an error is thrown
+    }
+        catch(const out_of_range&) //If larger than an int (32 bits), stoi throws oor (out_of_range)
+    {
+        //If no conversionc can be made, return 0
+        return 0;
+    }
+}
+
+
+
+
+
+/*
+  SOLUTION 2: Using integer arithmetic
+  ====================================
+*/
