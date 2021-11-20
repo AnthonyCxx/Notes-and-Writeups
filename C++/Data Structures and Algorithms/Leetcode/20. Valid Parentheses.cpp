1b39d2ac7bd73b8fd051316ae90392d810bcebab
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-11-20 10:49:11 -0500

    Update 20. Valid Parentheses.cpp

--- C++/Data Structures and Algorithms/Leetcode/20. Valid Parentheses.cpp
+++ C++/Data Structures and Algorithms/Leetcode/20. Valid Parentheses.cpp
@@ -1,1 +1,39 @@
 
+
+bool isValid(std::string s) 
+{
+    //Create a stack to store the left parentheses
+    std::stack<char> stk;
+
+    //For every character in the string
+    for (const char c : s)
+    {
+        //If an opening parenthesis, store it
+        if (c == '(' or c == '[' or c == '{')
+        {
+            stk.push(c);
+        }
+        else if (!stk.empty())   //if there are opening parenthesis to match with
+        {
+            if (stk.top() == '(' and c == ')')    //if ( and )
+            { 
+                stk.pop();
+            }
+            else if (stk.top() == '[' and c == ']')  //if [ and ]
+            {
+                stk.pop();
+            }
+            else if (stk.top() == '{' and c == '}')  //if { and }
+            {
+                stk.pop();
+            }
+            else //if most recent opening parenthesis does not match the closing parenthesis -> invalid string
+                return false;
+        }
+        else //if you encounter a closing parenthesis without having an opening parenthesis to match it with -> invalid string
+            return false;
+    }
+
+    //If there are no unclosed parentheses -> valid string
+    return stk.empty();
+}
