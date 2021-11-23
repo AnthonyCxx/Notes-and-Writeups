c9d31597c1588462a6d13faba6e9a8f9e934c53e
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-11-22 19:09:03 -0500

    Update 9. Palindrome Number.cpp

--- C++/Data Structures and Algorithms/Leetcode/9. Palindrome Number.cpp
+++ C++/Data Structures and Algorithms/Leetcode/9. Palindrome Number.cpp
@@ -1,1 +1,30 @@
 
+/*
+  Solution 1: Casting to std::string
+  ==================================
+  By casting the integer to a string, we can know the 
+  number of digits (now characters). From there, it's just
+  a matter of iterating over the string and making sure the
+  numbers match.
+*/
+bool isPalindrome(int x) 
+{
+    //Cast the integer to a string to determine the number of characters
+    std::string str = std::to_string(x);
+
+    //Mirrored iteration: make sure each number 'i' and 'j' match
+    for(std::size_t i=0, j=str.size()-1; i < j; ++i, --j)
+    {
+        if (str[i] != str[j])
+            return false;
+    }
+
+    //If all the checks were passed
+    return true;
+}
+};
+
+
+/*
+
+*/
