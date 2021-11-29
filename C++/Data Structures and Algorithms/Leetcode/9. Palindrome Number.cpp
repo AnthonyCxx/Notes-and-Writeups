5c604ba4f9bc242a863f1fd2bfe56cf19a51e5cb
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2021-11-28 19:44:54 -0500

    Update 9. Palindrome Number.cpp

--- C++/Data Structures and Algorithms/Leetcode/9. Palindrome Number.cpp
+++ C++/Data Structures and Algorithms/Leetcode/9. Palindrome Number.cpp
@@ -25,5 +25,30 @@ bool isPalindrome(int x)
 
 
 /*
-
+  Solution 2: Using Math (**shutters**)
+  =====================================
+  1. You can get the last number of an integer by modding it by 10
+  2. You can append a number to an integer by multiplying it by 10 and then adding the number
+  3. You can truncate the last number of an integer by dividing it by 10
 */
+bool isPalindrome(int x) 
+{
+    if (x < 0)
+        return false;
+
+    //Variables
+    int x_copy = x;         //Create a copy of x that can be manipulated 
+    long x_reversed = 0;   //Stores the reveresed value of x, which may exceed the max size of an int
+
+    //Until there are no numbers left in x
+    while (x_copy != 0)
+    {
+        //Append the last digit of x_copy to x_reveresed
+        x_reversed = x_reversed*10 + (x_copy % 10);
+
+        //Remove the last digit from the x_copy
+        x_copy /= 10;
+    }
+
+    return x == x_reversed;
+}
